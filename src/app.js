ab358fe905ab864e9599c5649c8d28b0658d586e
Giovannytrotta <giovannytrotta13@gmail.com>
2025-03-03 19:47:44 +0000

    change group

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let domain = document.getElementById("domain");
+const button = document.querySelector ("#generar");
+
+const pronoun = ['the', 'the','the','domain'];
+const adj = ['reward', 'Ebook','masterclass','proyection'];
+const noun = ['center', 'reward' , 'online','times' ];
+
+function generateDomain() {
+  let domain = "";
+  
+  pronoun.forEach(p =>
+    adj.forEach(a =>
+      noun.forEach(n => {
+        domain += `<li class="list-group-item">${p}${a}${n}.com</li>`;
+      })
+    )
+  );
+
+  console.log(domain); 
+  return domain; 
+  
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  button.addEventListener("click",function(){
+  domain.innerHTML = generateDomain();
+
+});
 };
+
+console.log(""); 
